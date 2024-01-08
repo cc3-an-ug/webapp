83887640ac9b50524af3abef941b6c3e3109a549
Andrés Castellanos <me@andrescv.dev>
2024-01-08 15:45:48 -0600

    feat: add shell to sanity code input options

--- src/schemas/content.ts
+++ src/schemas/content.ts
@@ -57,6 +57,7 @@ export const blockContent = defineType({
           { title: 'HTML', value: 'html' },
           { title: 'CSS', value: 'css' },
           { title: 'C', value: 'c' },
+          { title: 'Shell', value: 'sh' },
         ],
       },
     },
