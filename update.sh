43a0d93e53d9e04682d29220fbce0f0cdc1d0975
Andrés Castellanos <me@andrescv.dev>
2024-01-21 10:48:36 -0600

    chore: update dependencies

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 npx -y npm-check-updates@latest -u
 
-cd app && npx -y npm-check-updates@latest -u && cd ..
+cd apps
+cd cms && npx -y npm-check-updates@latest -u && cd ..
+cd docs && npx -y npm-check-updates@latest -u && cd ..
+cd platform && npx -y npm-check-updates@latest -u && cd ../..
 
 cd packages
 cd cc3-design && npx -y npm-check-updates@latest -u && cd ..
 cd cc3-tsconfig && npx -y npm-check-updates@latest -u && cd ..
 cd eslint-config-cc3 && npx -y npm-check-updates@latest -u && cd ../..
 
-echo "$PWD"
+echo "=> Done"
