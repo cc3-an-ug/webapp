5b48d94be1746b99da4ef8b3e6b293c58c27ca21
Andrés Castellanos <me@andrescv.dev>
2024-01-22 09:47:26 -0600

    feat: update docs middleware to match certain paths

--- apps/docs/src/middleware.ts
+++ apps/docs/src/middleware.ts
@@ -10,3 +10,16 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
